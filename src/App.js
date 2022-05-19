afdacfca7abd03be646496100534978745366a14
Nazarii Pirih <nazaryipyryg@gmail.com>
2022-05-19 21:45:58 +0300

    [#4] add a forgotpass page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
+import { ForgotPasswordPage } from "./pages/ForgotPasswordPage/ForgotPasswordPage";
 import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
-  return <div className="App">team</div>;
+  return (
+    <div className="App">
+      <ForgotPasswordPage />
+    </div>
+  );
 }
 
 export default App;
