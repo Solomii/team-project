15fcee29f88ccec89fcc6aacfed99f6eeb0b207c
Ihor Shalapa <igorshalapa@gmail.com>
2022-05-20 08:51:34 +0300

    add route to App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+
 import "bootstrap/dist/css/bootstrap.css";
 
+
 function App() {
-  return <div className="App">team</div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="" element={< />} />
+          <Route path="" element={< />} />
+          <Route element={< />}></Route>
+        </Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
