ca5feea7cfafd160347c852193b8c2e4d59d5eec
Ihor Shalapa <igorshalapa@gmail.com>
2022-05-20 12:26:50 +0300

    add route

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 
 import "bootstrap/dist/css/bootstrap.css";
+import HomePage from "./pages/HomePage/HomePage";
+import ProfilePage from "./pages/ProfilePage/ProfilePage";
+import MemberListPage from "./pages/MemberListPage/MemberListPage";
+import ManagerListPage from "./pages/ManagerListPage/ManagerListPage";
+import Layout from "./layout/Layout/Layout";
 
 
 function App() {
@@ -9,10 +14,12 @@ function App() {
     <div className="App">
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="" element={< />} />
-          <Route path="" element={< />} />
-          <Route element={< />}></Route>
+          <Route index element={<HomePage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/memberList" element={<MemberListPage />} />
+          <Route path="/managerList" element={<ManagerListPage />} />
+          <Route path="/eventList" element={<ManagerListPage />} />
+          <Route path="/event" element={<ManagerListPage />} />
         </Route>
       </Routes>
     </div>
