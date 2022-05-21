39fd299d731298f09f90eb150d9fd7622d9ff8e4
Nazarii Pirih <nazaryipyryg@gmail.com>
2022-05-21 20:20:45 +0300

    [#23] create add event form, create close button, create new input

--- src/ui/button/LoginButton/LoginButton.js
+++ src/ui/button/LoginButton/LoginButton.js
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from "../../../assets/scss/loginbutton.module.scss";
 
-export const LoginButton = ({ buttonText }) => {
+ const LoginButton = ({ buttonText }) => {
   return (
     <div>
       <button type="button" className={`btn btn-primary rounded-pill btn-lg fs-6 ${styles.loginButton}`}>
@@ -10,3 +10,4 @@ export const LoginButton = ({ buttonText }) => {
     </div>
   );
 }
+export default LoginButton;
