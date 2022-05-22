31754b5489b60a912ba1ffc8c42becaf98ddf7ef
Ihor Shalapa <igorshalapa@gmail.com>
2022-05-22 21:00:59 +0300

    change styles

--- src/ui/button/LoginButton/LoginButton.js
+++ src/ui/button/LoginButton/LoginButton.js
@@ -4,7 +4,7 @@ import styles from "../../../assets/scss/loginbutton.module.scss";
 export const LoginButton = ({ buttonText }) => {
   return (
     <div>
-      <button type="button" className={`btn btn-primary ${styles.loginButton}`}>
+      <button type="button" className={`btn rounded-pill btn-primary ${styles.loginButton}`}>
         {buttonText}
       </button>
     </div>
