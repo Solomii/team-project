a9b0e4e84a4f16688c006ef1498defbf84cf5742
Shaliapyn <83767590+Shaliapyn@users.noreply.github.com>
2022-05-23 13:19:09 +0300

    Correct tab (#24)
    add table wrapper

--- src/pages/ManagerList/ManagerList.js
+++ src/pages/ManagerList/ManagerList.js
@@ -1,56 +1,64 @@
 import React from "react";
 
-import  LoginButton  from "../../ui/button/LoginButton";
+import LoginButton from "../../ui/button/LoginButton";
 import styles from "../../assets/scss/memberlist.module.scss";
 
 const ManagerList = () => {
   return (
-    <div>
       <div className={`${styles.tableContainer}`}>
-        <div className={styles.flexBlock}>
-          <h2 style={{ fontSize: "18px" }}>Members List</h2>
-          <div style={{ width: "200px" }}>
-            <LoginButton btnClass="btn-danger" buttonText={"Add User"} />
+        <div className="card shadow mb-4">
+          <div className={styles.flexBlock}>
+            <h2 style={{ fontSize: "18px" }}>Members List</h2>
+            <div style={{ width: "200px" }}>
+              <LoginButton btnClass="btn-danger" buttonText={"Add User"} />
+            </div>
+          </div>
+          <div className="card-body px-5">
+            <table className={`table table-bordered shadow-sm`}>
+              <thead className={`table-light`}>
+                <tr>
+                  <th scope="col">Avatar</th>
+                  <th scope="col">First Name</th>
+                  <th scope="col">Last Name</th>
+                  <th scope="col">Organisation</th>
+                  <th scope="col">Email</th>
+                  <th scope="col">Phone</th>
+                  <th scope="col">Score</th>
+                  <th scope="col">Birth Date</th>
+                  <th scope="col">Managing</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr className={styles.tableRow}>
+                  <th scope="row">1</th>
+                  <td>Gustavus</td>
+                  <td>Adolphus</td>
+                  <td>Army</td>
+                  <td>@gmail</td>
+                  <td>+3806712494</td>
+                  <td>25</td>
+                  <td>24.05.1598</td>
+                  <td className={styles.btnBlock}>
+                    <LoginButton
+                      className={styles.memberBtn}
+                      buttonText={"Edit"}
+                    >
+                      Edit
+                    </LoginButton>
+                    <LoginButton
+                      className={styles.memberBtn}
+                      buttonText={"Delete"}
+                    >
+                      Delete
+                    </LoginButton>
+                  </td>
+                </tr>
+                {/* {Members} */}
+              </tbody>
+            </table>
           </div>
         </div>
-        <table className={`table table-bordered shadow`}>
-          <thead className={`table-light`}>
-            <tr>
-              <th scope="col">Avatar</th>
-              <th scope="col">First Name</th>
-              <th scope="col">Last Name</th>
-              <th scope="col">Organisation</th>
-              <th scope="col">Email</th>
-              <th scope="col">Phone</th>
-              <th scope="col">Score</th>
-              <th scope="col">Birth Date</th>
-              <th scope="col">Managing</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr className={styles.tableRow}>
-              <th scope="row">1</th>
-              <td>Gustavus</td>
-              <td>Adolphus</td>
-              <td>Army</td>
-              <td>@gmail</td>
-              <td>+3806712494</td>
-              <td>25</td>
-              <td>24.05.1598</td>
-              <td className={styles.btnBlock}>
-                <LoginButton className={styles.memberBtn} buttonText={"Edit"}>
-                  Edit
-                </LoginButton>
-                <LoginButton className={styles.memberBtn} buttonText={"Delete"}>
-                  Delete
-                </LoginButton>
-              </td>
-            </tr>
-            {/* {Members} */}
-          </tbody>
-        </table>
       </div>
-    </div>
   );
 };
 
--- src/pages/MemberList/MemberList.js
+++ src/pages/MemberList/MemberList.js
@@ -6,32 +6,35 @@ import styles from "../../assets/scss/memberlist.module.scss";
 const MemberList = () => {
   return (
     <div className={`${styles.tableContainer}`}>
-      <div className={styles.flexBlock}>
-        <h2 style={{ fontSize: "18px" }}>Members List</h2>
-        <div style={{ width: "200px" }}>
+      <div className="card shadow mb-4">
+        <div className={styles.flexBlock}>
+          <h2 style={{ fontSize: "18px" }}>Members List</h2>
+          <div style={{ width: "200px" }}></div>
+        </div>
+        <div className="card-body px-5">
+          <table className={`table table-bordered shadow-sm`}>
+            <thead className={`table-light ${styles.tableHead}`}>
+              <tr>
+                <th scope="col">Avatar</th>
+                <th scope="col">First Name</th>
+                <th scope="col">Last Name</th>
+                <th scope="col">Organisation</th>
+                <th scope="col">Phone</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope="row">1</th>
+                <td>Gustavus</td>
+                <td>Adolphus</td>
+                <td>Army</td>
+                <td>+3806712494</td>
+              </tr>
+              {/* {Members} */}
+            </tbody>
+          </table>
         </div>
       </div>
-      <table className={`table table-bordered shadow`}>
-        <thead className={`table-light ${styles.tableHead}`}>
-          <tr>
-            <th scope="col">Avatar</th>
-            <th scope="col">First Name</th>
-            <th scope="col">Last Name</th>
-            <th scope="col">Organisation</th>
-            <th scope="col">Phone</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <th scope="row">1</th>
-            <td>Gustavus</td>
-            <td>Adolphus</td>
-            <td>Army</td>
-            <td>+3806712494</td>
-          </tr>
-          {/* {Members} */}
-        </tbody>
-      </table>
     </div>
   );
 };
